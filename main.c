b0aa40065c1c648e6431ac1dffb8ba93d99346d6
Apurva <apurvarai99@gmail.com>
2020-03-28 21:18:00 -0500

    Output to terminal in main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+//Project: Quite a Shell (Quash)
+//Made by Apurva Rai and Meet Kapadia
+
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <algorithm>
+#include <time.h>
+
+#include "changeDirectory.c"
+#include "executive.c"
+
+using namespace std;
+
+int main (int argc, char **argv, char **envp)
+{
+
+printf("\e[1;1H\e[2J"); //Clear console
+printf("\t\tQuite a Shell\n")
+
+}
